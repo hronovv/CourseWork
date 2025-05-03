d0f90bdbafc98e86ec007cdb129b82eed1d3e6f0
hronovv <ivanonlane@gmail.com>
2025-05-03 13:28:48 +0300

    Create common.h

---
+++ include/common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/*
+ * This file contains all includes that are needed for the course work
+*/
+
+#include <QApplication>
+#include <QPushButton>
+#include "QCheckBox"
+#include <QStackedWidget>
+#include <QVBoxLayout>
+#include "QGridLayout"
+#include <QLabel>
+#include <iostream>
+#include <memory>
+
+
+#endif //COMMON_H
